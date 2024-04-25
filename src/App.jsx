1485befaa625baf7e1269774132890b0a9e6cdbf
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2024-04-25 20:17:25 -0300

    adicionando opção de fullscreen

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ export default function App() {
 
           {popupOpen && (
             <div className='fixed top-[20%] left-[6%] z-[2000] '>
-              <iframe className='w-[90vw] h-[60vh] rounded-xl border-none relative' src={iframeLink} frameborder="0"></iframe>
+              <iframe className='w-[90vw] h-[60vh] rounded-xl border-none relative' src={iframeLink} frameborder="0" allowFullScreen />
               <button className='absolute top-0 right-0 text-3xl z-[1000] border-none mt-[10px] mr-3 cursor-pointer bg-black p-[14px] text-white hover:bg-red-600 rounded-md transition-all' onClick={closePopup}>X</button>
             </div>
           )}
