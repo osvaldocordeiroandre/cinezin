24b52fc5c165a5940d2e746168528fdecb0550d5
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2024-05-24 23:51:26 -0300

    mudando velocidade da animação to scroltop

--- src/Components/scrolltotop/ScrollTop.jsx
+++ src/Components/scrolltotop/ScrollTop.jsx
@@ -30,7 +30,7 @@ export default function ScrollTop() {
         <>
             <div
                 className='fixed bottom-10 right-10 z-[2px] cursor-pointer w-10 h-10 border-none shadow-lg bg-orange-500 rounded-lg hover:bg-orange-600 transition-all flex justify-center items-center'
-                style={{ transform: `scale(${backTop ? 1 : 0})`, transition: 'transform .2s ' }}
+                style={{ transform: `scale(${backTop ? 1 : 0})`, transition: 'transform .1s ' }}
                 onClick={scrollUp}
             >
                 <svg width="28" height="28" fill="white" viewBox="0 0 256 256"><path d="M222.138,91.475l-89.6-89.6c-2.5-2.5-6.551-2.5-9.051,0l-89.6,89.6c-2.5,2.5-2.5,6.551,0,9.051s6.744,2.5,9.244,0L122,21.85  V249.6c0,3.535,2.466,6.4,6,6.4s6-2.865,6-6.4V21.85l78.881,78.676c1.25,1.25,2.992,1.875,4.629,1.875s3.326-0.625,4.576-1.875  C224.586,98.025,224.638,93.975,222.138,91.475z"></path></svg>
