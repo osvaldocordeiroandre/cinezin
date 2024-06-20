6bd9fe726a57e71e9485d0cb9153627e2041c775
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2024-06-20 03:29:13 -0300

    transformando botão em componente para deixar o código mais clean

--- src/Components/filterpopup/FilterPopup.jsx
+++ src/Components/filterpopup/FilterPopup.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import InfoButtons from "../../assets/buttonsinfo/ButtonsInfo";
 
 export default function FilterPopup({
   filter,
@@ -14,78 +14,15 @@ export default function FilterPopup({
           <div className="fixed top-[16%] bg-zinc-800 w-[96vw] h-[70vh] p-4 rounded-md  overflow-y-auto">
             <div className="flex justify-between items-center">
               <div className="pb-3 cd:w-[500px]  space-y-2 space-x-1">
-                <button
-                  onClick={() => setSelectGenero("")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Todos
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Comédia")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Comédia
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Infantil")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Infantil
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Ação")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Ação
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Drama")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Drama
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Fantasia")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Fantasia
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Musical")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Musical
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Aventura")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Aventura
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Thriller")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Thriller
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Crime")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Crime
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Ficção científica")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Ficção científica
-                </button>
-                <button
-                  onClick={() => setSelectGenero("Terror")}
-                  className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
-                >
-                  Terror
-                </button>
+                {InfoButtons.map((butto) => (
+                  <button
+                    key={butto.id}
+                    className="text-white bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600 transition-all"
+                    onClick={() => setSelectGenero(`${butto.genefilter}`)}
+                  >
+                    {butto.text}
+                  </button>
+                ))}
               </div>
               <button
                 className="text-base z-[1000] border-none cursor-pointer bg-black p-[14px] text-white hover:bg-red-600 rounded-md transition-all"
---
+++ src/assets/buttonsinfo/ButtonsInfo.jsx
@@ -0,0 +1,64 @@
+const InfoButtons = [
+  {
+    id: 1,
+    text: "Todos",
+    genefilter: "",
+  },
+  {
+    id: 2,
+    text: "Comédia",
+    genefilter: "Comédia",
+  },
+  {
+    id: 3,
+    text: "Infantil",
+    genefilter: "Infantil",
+  },
+  {
+    id: 4,
+    text: "Ação",
+    genefilter: "Ação",
+  },
+  {
+    id: 5,
+    text: "Drama",
+    genefilter: "Drama",
+  },
+  {
+    id: 6,
+    text: "Fantasia",
+    genefilter: "Fantasia",
+  },
+  {
+    id: 7,
+    text: "Musical",
+    genefilter: "Musical",
+  },
+  {
+    id: 8,
+    text: "Aventura",
+    genefilter: "Aventura",
+  },
+  {
+    id: 9,
+    text: "Thriller",
+    genefilter: "Thriller",
+  },
+  {
+    id: 10,
+    text: "Crime",
+    genefilter: "Crime",
+  },
+  {
+    id: 11,
+    text: "Ficção científica",
+    genefilter: "Ficção científica",
+  },
+  {
+    id: 12,
+    text: "Terror",
+    genefilter: "Terror",
+  },
+];
+
+export default InfoButtons;
