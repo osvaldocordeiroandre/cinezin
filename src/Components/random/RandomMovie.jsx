5a9055a26711126445a5da5940a1c6b3a014ae3c
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2024-06-25 22:45:52 -0300

    arrumando rename da area de random vídeos

--- src/Components/random/RandomMovie.jsx
+++ src/Components/random/RandomMovie.jsx
@@ -7,12 +7,12 @@ import Movie from '../../assets/db/capas'
 export default function RandomMovie({openPopup}) {
     const handleMovie = () => {
         const randomIndex = Math.floor(Math.random() * Movie.length);
-        const randomMovieSrc = Movie[randomIndex].link;
+        const randomMovieSrc = Movie[randomIndex].video_url;
         openPopup(randomMovieSrc);
     }
     return (
         <>
-            <GiPerspectiveDiceSixFacesRandom onClick={handleMovie} className='cursor-pointer' size={30} color='white' />
+            <GiPerspectiveDiceSixFacesRandom onClick={handleMovie} className='cursor-pointer hover:fill-orange-400 duration-300 transition-all' size={30} color='white' />
         </>
     )
 }
