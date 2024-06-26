c5571a32512f2c70193fc7040763a0275644be0e
Osvaldo Cordeiro André <94922391+osvaldocordeiroandre@users.noreply.github.com>
2024-06-26 06:37:00 -0300

    ajustando barra de filtrar e mudando a cor do botao de scroll to top

--- src/Components/filterpopup/FilterPopup.jsx
+++ src/Components/filterpopup/FilterPopup.jsx
@@ -4,7 +4,7 @@ export default function FilterPopup({ filter, selected }) {
   return (
     <div
       className={`bg-zinc-800 w-52 rounded-md z-10 flex flex-col justify-center items-center ${
-        filter ? "translate-y-[460px]" : "translate-y-[140%]"
+        filter ? "translate-y-[470px]" : "translate-y-[140%]"
       } ${filter ? "visible" : "invisible"} ${
         filter ? "opacity-100" : "opacity-0"
       } duration-700 transition-all text-white absolute top-[330px] right-2 p-4 gap-2 sm:top-[450px] mp:top-[480px]`}
